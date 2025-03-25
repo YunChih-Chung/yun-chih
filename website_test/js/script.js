69f788dede9867490eac7ac973e0221f207f6a22
YunChih-Chung <134210411+YunChih-Chung@users.noreply.github.com>
2025-03-26 02:37:49 +0800

    Add files via upload

---
+++ website_test/js/script.js
@@ -0,0 +1,6 @@
+// 切換顯示/隱藏表格
+function toggleTable() {
+    const table = document.getElementById('competitionTable');
+    table.style.display = table.style.display === 'none' ? 'table' : 'none';
+  }
+  
