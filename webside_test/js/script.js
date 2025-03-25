53f34fa1bcb16a19a08f0f2a1fddf25646f52a00
YunChih-Chung <134210411+YunChih-Chung@users.noreply.github.com>
2025-03-26 02:37:14 +0800

    Delete webside_test directory

--- webside_test/js/script.js
+++
@@ -1,6 +0,0 @@
-// 切換顯示/隱藏表格
-function toggleTable() {
-    const table = document.getElementById('competitionTable');
-    table.style.display = table.style.display === 'none' ? 'table' : 'none';
-  }
-  
